69d3344a27d27a317939f44a96ebca99d422095d
Fatma <fatmahussein355@gmail.com>
2024-03-18 11:45:50 +0300

    week2_plp

---
+++ list.py
@@ -0,0 +1,15 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(2, 15)
+print(my_list)
+list = [50, 60, 70]
+my_list.extend(list)
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+index = my_list.index(30)
+print("The index of 30 is:", index)
